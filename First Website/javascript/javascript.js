12f9e2d6df2a4265751b2ec2b3244d56d6217d23
tianasser <99832147+tianasser@users.noreply.github.com>
2022-04-13 17:36:32 -0400

    Add files via upload

---
+++ First Website/javascript/javascript.js
@@ -0,0 +1,12 @@
+
+$(".hidden").click(function() {
+
+    if ($(".hidden").hasClass("hidden")) {
+        $(".hidden").addClass("visible")} 
+        
+        /*else {
+        $(".menu-area").addClass("active")
+    }
+
+    $(".menu-area").toggleClass("active")*/
+})
